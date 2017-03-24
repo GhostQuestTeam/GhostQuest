da3aed849b65d8cf4e8114bdad051b712365cc74
Danil Cherepanov <dancheran@yandex.ru>
2017-03-24 20:16:14 +0300

    Отрисовка одногоузла диалога

--- Assets/DialogueSystem/DialogueView.cs
+++ Assets/DialogueSystem/DialogueView.cs
@@ -1,9 +1,41 @@
 ﻿
+using UnityEngine;
+using UnityEngine.UI;
 
 //Отображает содержимое DialogNode на экран
-namespace DialogueSystem
+namespace DialogueSystem 
 {
-    public class DialogueView  {
-    
+	public class DialogueView : MonoBehaviour  {
+		public GameObject answerPrefab;
+		DialogueGraph dialogue;
+
+		void Start(){
+		}
+
+		void Update(){
+		}
+
+		public DialogueGraph Dialogoue{
+			get{
+				return this.dialogue;
+			}
+			set{
+				dialogue = value;
+				_updateView ();
+			}
+		}
+
+		private void  _updateView(){
+            var answersPanel =transform.Find ("AnswersPanel");
+            var invitation = transform.Find("Invitation Panel");
+
+            invitation.GetComponentInChildren<Text>().text = dialogue.CurrentNode.Invitation;
+
+            foreach (var answer in dialogue.CurrentNode.Answers) {
+				var answerButton = (GameObject) Instantiate (answerPrefab);
+                answerButton.GetComponentInChildren<Text>().text = answer.Message;
+                answerButton.transform.SetParent (answersPanel);
+			}
+		}
     }
 }
