a012bd3856c7ab3cc24f77d64d7eb2d9bbd52c13
Alexander <Alex170995@yandex.ru>
2017-03-25 18:17:17 +0300

    events and GPS

---
+++ Assets/ControlSystem/BasicNotification/GPSNotification/GPSConsumer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSConsumer : MonoBehaviour, 
+INotificationConsumer<GPSNotifier, GPSNotifier.GPSDataEventArgs> {
+
+	public GameObject oGPSNotifier;
+	GPSNotifier gpsNotifier;
+	bool firstNotification;
+	GPSNotifier.GPSDataEventArgs prevArgs;
+
+	// Use this for initialization
+	void Start () {
+		firstNotification = true;
+		gpsNotifier = oGPSNotifier.GetComponent<GPSNotifier> ();
+		gpsNotifier.emitter += new ConsumerDelegate<GPSNotifier, GPSNotifier.GPSDataEventArgs> (Consume);
+	}
+	
+	public void Consume (GPSNotifier notifier, GPSNotifier.GPSDataEventArgs args) {
+		if (firstNotification) {
+			prevArgs = args;
+			firstNotification = false;
+		}
+		if (args == prevArgs) {
+			return;
+		}
+		//here smth useful
+		Debug.Log (args.Latitude + " " + args.Longitude + " " + args.Altitude + " ");
+	}
+
+}
