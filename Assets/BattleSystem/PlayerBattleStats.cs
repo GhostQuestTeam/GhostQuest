d63dee6159a79f9621f411f5151dc962f4c4e46a
Danil Cherepanov <dancheran@yandex.ru>
2017-04-15 20:24:00 +0300

    Добавлен класс, отображающий здоровье и энергию игрока

---
+++ Assets/BattleSystem/PlayerBattleStats.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Utils;
+
+namespace BattleSystem
+{
+    public class PlayerBattleStats
+    {
+        private BoundedInt _energy;
+
+        public Solidity Solidity { get; set; }
+        public int EnergyRegen { get; set; }
+        public int MaxEnergy
+        {
+            get { return _energy.Max; }
+            set { _energy.Max = value; }
+        }
+
+        public int CurrentEnergy
+        {
+            get { return _energy.Val; }
+            set { _energy.Val = value; }
+        }
+
+        public uint MaxHealth
+        {
+            get { return Solidity.MaxHealth; }
+            set { Solidity.MaxHealth = value; }
+        }
+
+        public int CurrentHealth => Solidity.CurrentHealth;
+
+        public PlayerBattleStats(Solidity solidity, int maxEnergy, int energyRegen)
+        {
+            Solidity = solidity;
+            _energy = new BoundedInt(maxEnergy, 0, maxEnergy);
+            EnergyRegen = energyRegen;
+        }
+
+    }
+}
