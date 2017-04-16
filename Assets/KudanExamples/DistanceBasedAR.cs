9035144ca6d55e89e35f07d4e353f834c7c78c79
Alexander <Alex170995@yandex.ru>
2017-04-16 04:38:07 +0300

    kudan

---
+++ Assets/KudanExamples/DistanceBasedAR.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceBasedAR : MonoBehaviour {
+
+    public GPSProvider gpsProvider;
+    public TrivialKudanApp ka;
+    public Camera cam;
+
+    private bool isGPSReadFirstTime = true;
+
+    private Vector3 startGPS;
+    private Vector3 curGPS;
+
+    public bool isTracking = false;
+    private Vector3 trackingPosition = new Vector3(65, -20, 2000);
+
+    public float _debug_DeltaGPS;
+
+    // Use this for initialization
+    void Start () {
+        trackingPosition = cam.transform.forward * 50;
+        ka.StartMarkerlessTracking(trackingPosition, Quaternion.identity);
+        isTracking = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float lat = gpsProvider.Latitude;
+        float lon = gpsProvider.Longitude;
+        float alt = gpsProvider.Altitude;
+
+        if ((lat == 0) || (lon == 0)) //low error probability
+        {
+            return;
+        }
+
+        if (isGPSReadFirstTime)
+        {
+            startGPS = new Vector3(lat, lon, alt);
+            curGPS = new Vector3(lat, lon, alt);
+            isGPSReadFirstTime = false;
+            return;
+        }
+
+        curGPS = new Vector3(lat, lon, alt); //update current position
+        float deltaGPSval = (curGPS - startGPS).magnitude; //delta position between prev and current read
+        float gpsScaleFactor = 100000f;
+        float deltaGPSCut = 10 / gpsScaleFactor;
+
+        _debug_DeltaGPS = deltaGPSval * gpsScaleFactor; //for debug output
+
+        if (deltaGPSval < deltaGPSCut) //if we moved too short - ok, show if not showing
+        {
+            if(!isTracking)
+            {
+                ka.StartMarkerlessTracking(trackingPosition, Quaternion.identity);
+                isTracking = true;
+            }
+            return;
+        }
+        else //else stop tracking
+        {
+            ka.StopMarkerlessTracking();
+            isTracking = false;
+        }
+        
+    }//update
+
+}//class
---
+++ Assets/KudanExamples/TrivialKudanApp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kudan.AR;
+
+public class TrivialKudanApp : MonoBehaviour {
+
+    public KudanTracker _kudanTracker;  // The tracker to be referenced in the inspector. This is the Kudan Camera object.
+    public TrackingMethodMarker _markerTracking;    // The reference to the marker tracking method that lets the tracker know which method it is using
+    public TrackingMethodMarkerless _markerlessTracking;	// The reference to the markerless tracking method that lets the tracker know which method it is using
+
+    void Start()
+    {
+        _kudanTracker.ChangeTrackingMethod(_markerlessTracking);
+    }
+
+    public void StartMarkerlessTracking(Vector3 pos, Quaternion orient)
+    {
+        _kudanTracker.ChangeTrackingMethod(_markerlessTracking);
+        //_kudanTracker.ArbiTrackStart(pos, orient);              // Starts markerless tracking based upon the given floor position and orientations
+
+        // from the floor placer.
+        Vector3 floorPosition;          // The current position in 3D space of the floor
+        Quaternion floorOrientation;    // The current orientation of the floor in 3D space, relative to the device
+
+        _kudanTracker.FloorPlaceGetPose(out floorPosition, out floorOrientation);   // Gets the position and orientation of the floor and assigns the referenced Vector3 and Quaternion those values
+        _kudanTracker.ArbiTrackStart(floorPosition, floorOrientation);
+    }
+
+    public void StopMarkerlessTracking()
+    {
+        _kudanTracker.ArbiTrackStop();
+    }
+
+}
