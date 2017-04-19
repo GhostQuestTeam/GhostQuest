4c1e07c797817bddb5037e64ecba976a5c68e72b
Danil Cherepanov <dancheran@yandex.ru>
2017-04-20 01:39:54 +0300

    Интеграция выстрела и смены оружия с новым интерфейсом

---
+++ Assets/BattleSystem/BattleInterfaceController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BattleSystem
+{
+    public class BattleInterfaceController : MonoBehaviour
+    {
+        private PlayerBattleController _battleController;
+
+
+        void Start()
+        {
+            _battleController = GameObject.FindWithTag("Player").GetComponent<PlayerBattleBehavior>().BattleController;
+            var weapons = _battleController.BattleStats.Weapons;
+
+            transform.Find("ButtonShoot").GetComponent<Button>().onClick.AddListener(()=> _battleController.TryShoot());
+
+            //TODO Обобщить и не писать как здесь всё в одну строку
+            transform.Find("WeaponsPanel/BlasterButton").GetComponent<Button>().onClick.AddListener(() => _battleController.BattleStats.CurrentWeaponId = 1);
+            transform.Find("WeaponsPanel/BulletButton").GetComponent<Button>().onClick.AddListener(() => _battleController.BattleStats.CurrentWeaponId = 0);
+
+
+
+        }
+    }
+}
