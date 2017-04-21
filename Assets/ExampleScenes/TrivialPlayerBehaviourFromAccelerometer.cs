eb01af20340c8b34c3fc228b1b69afec5aab85ce
Alexander <Alex170995@yandex.ru>
2017-04-21 05:13:40 +0300

    Акселерометр и GPS (не тестировалось)
    Добавлено тривиальное и нетривиальное перемещение по акселерометру.
    Нетривиальное включает детектирование направления вперед/назад с
    гашением обратного ускорения, а также гашение ускорения от вращений.
    Используется неоптимизированный код, сохраняющий показатели
    акселерометра и угловых ускорений вращения по осям от гироскопа, а также
    другие показатели, производные по указанным, за несколько последних
    фреймов. Гашение вращения происходит по ограничению разности энергий
    акселерометра и гироскопа, перемещение - по усредненной проекции
    ускорения на ось z с отсечением малых значений обратных ускорений.
    Пример с GPS - в начале задается опорная точка в начальном положении
    игрока. Затем постоянно считываются данные GPS, и, когда расстояние от
    игрока до начальной точки превысит определенное значение, должна проасть
    модель. Когда расстояние станет меньше - снова появиться.

---
+++ Assets/ExampleScenes/TrivialPlayerBehaviourFromAccelerometer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrivialPlayerBehaviourFromAccelerometer : MonoBehaviour {
+
+    public GameObject cameraContainer;
+    public float moveSpeed = 30;
+
+    private GameObject cameraObject;
+    private CharacterController characterController;
+    private Camera cam;
+    private GyroControl gyroControl;
+
+    // Use this for initialization
+    void Start()
+    {
+        characterController = cameraContainer.GetComponent<CharacterController>();
+        cameraObject = cameraContainer.transform.GetChild(0).gameObject;
+        cam = cameraObject.GetComponent<Camera>();
+        gyroControl = cameraContainer.GetComponent<GyroControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        doAccelerometerMovement();
+    }
+
+
+    void doAccelerometerMovement()
+    {
+        if (!gyroControl.gyroEnabled)
+        {
+            return;
+        }
+
+        Vector3 userAcc = gyroControl.gyro.userAcceleration;
+        Vector3 moveVector = cameraObject.transform.forward * userAcc.magnitude * moveSpeed;
+        moveVector *= Time.deltaTime;
+        characterController.Move(moveVector);
+    }
+
+
+}
