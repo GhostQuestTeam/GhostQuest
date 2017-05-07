9e1fcc72f7145cd69e4cb3e38258d3b7b26e8d8f
Danil Cherepanov <dancheran@yandex.ru>
2017-05-07 23:34:07 +0300

    Исправление порядка отрисовки

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -9,15 +9,19 @@ public class UIController : MonoBehaviour
     public const string DEFAULT_DIALOGUE_PANEL_PREFAB = "DialoguesUI/DialoguePanel";
     public const string DEFAULT_QUEST_PANEL_PREFAB = "QuestsUI/QuestPanel";
     public const string DEFAULT_UI_PANEL_PREFAB = "UIFightPanel";
+    public const string DEFAULT_START_BATTLE_PREFAB = "BattleSystem/UI/StartBattleButton";
 
     public string DialoguePanelPrefabPath = DEFAULT_DIALOGUE_PANEL_PREFAB;
     public string QuestPanelPrefabPath = DEFAULT_QUEST_PANEL_PREFAB;
     public string UIPanelPrefabPath = DEFAULT_UI_PANEL_PREFAB;
+    public const string StartBattlePrefabPath = DEFAULT_START_BATTLE_PREFAB;
 
     private GameObject _dialoguePanel;
     private GameObject _questPanel;
     private GameObject _uiPanel;
     private Transform _canvas;
+
+    private GameObject _startBattleButton;
 
     // Use this for initialization
     void Start()
@@ -27,7 +31,10 @@ public class UIController : MonoBehaviour
         _initNavBar();
         _initDialoguePanel();
         _initQuestPanel();
+        _initStartBattleButton();
         _initEvents();
+
+
 
         _canvas.GetComponent<Canvas>().worldCamera = Camera.main;
     }
@@ -35,6 +42,14 @@ public class UIController : MonoBehaviour
     public GameObject DialoguePanel
     {
         get { return _dialoguePanel; }
+    }
+
+    private void _initStartBattleButton()
+    {
+        _startBattleButton = Instantiate(Resources.Load(StartBattlePrefabPath) as GameObject);
+        _startBattleButton.transform.SetParent(_canvas);
+        var gameController = GameObject.Find("GameController").GetComponent<GameController>();
+        _startBattleButton.GetComponent<Button>().onClick.AddListener(() => gameController.StartBattle());
     }
 
     private void _initDialoguePanel()
