90ec91bf2153242984c5663f80a29265d0224f16
Danil Cherepanov <dancheran@yandex.ru>
2017-05-12 13:18:12 +0300

    Панель с отображением уровня и опыта

--- Assets/Mapbox/Examples/LocationProvider/Scripts/MapUIController.cs
+++ Assets/Mapbox/Examples/LocationProvider/Scripts/MapUIController.cs
@@ -6,22 +6,37 @@ public class MapUIController:MonoBehaviour
 {
     private GameObject _attributesPanel;
     private GameObject _skillsButton;
+    private GameObject _levelPanel;
+
     void Start()
     {
         _attributesPanel = GameObject.Find("AttributesPanel");
         _skillsButton = GameObject.Find("Skills");
+        _levelPanel = GameObject.Find("LevelPanel");
 
-
+        _UpdateLevelView();
         _skillsButton.GetComponent<Button>().onClick.AddListener(() =>
         {
             _attributesPanel.SetActive(true);
             _skillsButton.SetActive(false);
+            _levelPanel.SetActive(false);
         });
 
         _attributesPanel.GetComponent<AttributesView>().OnClose += CloseHandler;
 
         _attributesPanel.SetActive(false);
 
+    }
+
+    private void _UpdateLevelView()
+    {
+        var level = GameController.GameStats.Level;
+        var currentExp = GameController.GameStats.CurrentExp;
+        var expToLevel = GameController.GameStats.ExpToLevel;
+
+        GameObject.Find("Level").GetComponent<Text>().text = level.ToString();
+        GameObject.Find("Exp").GetComponent<Text>().text = currentExp + "/" + expToLevel;
+        GameObject.Find("ExpBar").GetComponent<Image>().fillAmount = (float)currentExp/ expToLevel;
     }
 
     private void OnDestroy()
@@ -33,5 +48,6 @@ public class MapUIController:MonoBehaviour
     {
         _attributesPanel.SetActive(false);
         _skillsButton.SetActive(true);
+        _levelPanel.SetActive(true);
     }
 }
