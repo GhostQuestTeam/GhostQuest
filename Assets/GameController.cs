0d96921b917612c99193d7e66e21e3b15f9600fa
Danil Cherepanov <dancheran@yandex.ru>
2017-05-12 15:52:07 +0300

    Несколько новых врагов и рандомизированнвая генереция списка врагов на точке

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -9,7 +9,8 @@ public class GameController:MonoBehaviour
 
     private BattleStateController _battleStateController;
 
-
+    public string[] AllowableGhosts = { "shadow_skull", "devil_mask"};
+//"skull_ghost", "white_skull",
     void Awake() {
         DontDestroyOnLoad(transform.gameObject);
         _battleStateController = GameObject.Find("BattleStateController").GetComponent<BattleStateController>();
@@ -33,8 +34,18 @@ public class GameController:MonoBehaviour
         }
         if (scene.buildIndex == 2)
         {
-            _battleStateController.StartBattle(new Dictionary<string, int>(){{"skull_ghost", 5}});
+            _battleStateController.StartBattle(RandomGhosts()); //new Dictionary<string, int>(){{"skull_ghost", 5}});
         }
+    }
+
+    public Dictionary<string, int> RandomGhosts()
+    {
+        var result = new Dictionary<string, int>();
+        foreach (var ghost in AllowableGhosts)
+        {
+            result.Add(ghost, Random.Range(3,5));
+        }
+        return result;
     }
 
 
