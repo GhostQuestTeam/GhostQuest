4ef4fc7811be0b37ebd06d79e833a6ad13b0960c
Danil Cherepanov <dancheran@yandex.ru>
2017-05-26 18:01:27 +0300

    Registration UI

---
+++ Assets/Networking/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Networking
+{
+    public class AuthController
+    {
+        private static readonly AuthController instance = new AuthController();
+
+        private AuthController()
+        {
+        }
+
+        public static AuthController Instance
+        {
+            get { return instance; }
+        }
+
+        public void Register()
+        {
+            new GameSparks.Api.Requests.RegistrationRequest()
+                .SetDisplayName("Randy")
+                .SetPassword("test_password_123456")
+                .SetUserName("Test User 1")
+                .Send((response) =>
+                    {
+                        if (!response.HasErrors)
+                        {
+                            Debug.Log("Player Registered");
+                        }
+                        else
+                        {
+                            Debug.Log("Error Registering Player");
+                        }
+                    }
+                );
+        }
+    }
+}
---
+++ Assets/Networking/GameSparksManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Networking
+{
+    public class GameSparksManager : MonoBehaviour
+    {
+        private static GameSparksManager _instance = null;
+        void Awake() {
+            if (_instance == null)
+            {
+                _instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            } else
+            {
+                Destroy(this.gameObject);
+            }
+            AuthController.Instance.Register();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
