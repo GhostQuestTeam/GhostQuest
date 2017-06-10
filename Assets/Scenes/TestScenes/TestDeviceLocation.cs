3896bbc4170de4f1799d3c28ea3ddba59be58298
Alexander <Alex170995@yandex.ru>
2017-06-10 17:30:40 +0300

    Добавлена надпись с координатами.

---
+++ Assets/Scenes/TestScenes/TestDeviceLocation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mapbox.Unity.Location;
+using Mapbox.Utils;
+
+public class TestDeviceLocation : MonoBehaviour {
+
+    public Text locText;
+    private ILocationProvider lp;
+
+    // Use this for initialization
+    void Start () {
+        LocationProviderFactory lpf = gameObject.GetComponent<LocationProviderFactory>();
+        lp = lpf.DefaultLocationProvider;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2d pos = lp.Location;
+        locText.text = "Lat: " + pos.x + "  Lon: " + pos.y;
+	}
+}
