ca229e8fce9e741bfa4266568ea4782da23cc544
Danil Cherepanov <dancheran@yandex.ru>
2017-06-21 01:30:02 +0300

    Начата модификация боевой системы

--- Assets/Scripts/GameMechanics/BattleSystem/EnemyBehavior.cs
+++ Assets/Scripts/GameMechanics/BattleSystem/EnemyBehavior.cs
@@ -1,42 +1,90 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace HauntedCity.GameMechanics.BattleSystem
 {
+    [RequireComponent(typeof(Animator))]
     public class EnemyBehavior : MonoBehaviour
     {
+        public enum AttackType
+        {
+            Melee,
+            Range,
+            Kamikaze
+        }
+        
         private GameObject _followee;
+        private Animator _animator;
 
+        public AttackType attackType = AttackType.Kamikaze;
         public EnemyBattleController BattleController { get; private set; }
         public EnemyBattleStats BattleStats;
         public float DeathDelay = 0f;
+        public float AttackRange = 0.05f;
+        public float AttackCooldown = 1f;
         public int Score = 100;
+      
+
 
         // Use this for initialization
         void Awake()
         {
+            _animator = GetComponent<Animator>();
             _followee = GameObject.FindWithTag("Player");
             BattleStats.ResetHealth();
             BattleController = new EnemyBattleController(BattleStats);
             BattleController.OnDeath += () => StartCoroutine(Kill(DeathDelay));
+            BattleController.OnDamage += (damage) => _animator.SetTrigger("Hit");
+        }
+
+        private void Start()
+        {
+            StartCoroutine(AttackLoop());
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (_followee != null)
+            transform.LookAt(_followee.transform);
+            var distance = Vector3.Distance(transform.position, _followee.transform.position);
+            if (distance <= AttackRange)
             {
-                transform.LookAt(_followee.transform);
                 transform.position = Vector3.MoveTowards(transform.position, _followee.transform.position,
                     BattleStats.Velocity);
             }
-            else
-            {
-                Debug.logger.Log(gameObject.name + ": my followee is null!!!");
-            }
         }
 
+        IEnumerator AttackLoop()
+        {
+            while (true)
+            {
+                var distance = Vector3.Distance(transform.position, _followee.transform.position);
+                if (distance > AttackRange) continue;
+                
+                var player = _followee.GetComponent<PlayerBattleBehavior>();
+                _animator.SetTrigger("Attack");
+                switch (attackType)
+                {
+                    case AttackType.Melee:                            
+                        player.BattleController.TakeDamage(BattleStats.Damage);
+                        break;
+                    case AttackType.Kamikaze:
+                        player.BattleController.TakeDamage(BattleStats.Damage);
+                        BattleController.Kill();
+                        break;
+                    case AttackType.Range:
+                        //TODO
+                        break;
+                            
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                    
+                yield return new WaitForSeconds(AttackCooldown);
+            }
+        }
 
         public void OnTriggerEnter(Collider other)
         {
@@ -45,18 +93,19 @@ namespace HauntedCity.GameMechanics.BattleSystem
             {
                 BattleController.TakeDamage(shell.Weapon.Damage);
             }
-
-            if (other.gameObject.CompareTag("Player"))
-            {
-                var player = other.gameObject.GetComponent<PlayerBattleBehavior>();
-                player.BattleController.TakeDamage(BattleStats.Damage);
-                BattleController.Kill();
-            }
+//
+//            if (other.gameObject.CompareTag("Player"))
+//            {
+//                var player = other.gameObject.GetComponent<PlayerBattleBehavior>();
+//                player.BattleController.TakeDamage(BattleStats.Damage);
+//                BattleController.Kill();
+//            }
         }
 
         public IEnumerator Kill(float delay)
         {
             BattleStats.Velocity = 0;
+            _animator.SetTrigger("Die");
             yield return new WaitForSeconds(delay);
             Destroy(gameObject);
         }
