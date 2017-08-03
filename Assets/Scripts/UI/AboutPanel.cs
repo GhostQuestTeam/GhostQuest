06db37c520dfbb8fc60d4e4ec7f522e1a5ad728d
Ivan Baranov <gzintzfirmltd@mail.ru>
2017-08-03 09:06:50 +0400

    Added panel for About button
    AboutPanel under Canvas of start_scene created.
    Simple callback implemented.
    Need check for good integration with authorization system.

---
+++ Assets/Scripts/UI/AboutPanel.cs
@@ -0,0 +1,40 @@
+﻿using HauntedCity.Networking;
+using UnityEngine;
+using Zenject;
+
+namespace HauntedCity.UI
+{
+    public class AboutPanel : Panel
+    {
+        public GameObject[] ShowOnlyLoggedUser;
+        public GameObject[] ShowOnlyNotLoggedUser;
+
+
+        private void Start()
+        {
+        }
+
+        protected override void OnShow()
+        {
+            ShowMenu(AuthService.Instance.IsAuthenticated);
+        }
+
+        public void ShowMenu(bool isAuthenticated)
+        {
+            foreach (var item in ShowOnlyLoggedUser)
+            {
+                item.SetActive(isAuthenticated);
+            }
+            foreach (var item in ShowOnlyNotLoggedUser)
+            {
+                item.SetActive(!isAuthenticated);
+            }
+        }
+
+        public void Logout()
+        {
+            AuthService.Instance.Logout();
+            ShowMenu(AuthService.Instance.IsAuthenticated);
+        }
+    }
+}
