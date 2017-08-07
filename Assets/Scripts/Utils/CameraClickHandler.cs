db4ca887daf7169f8f5aaed1bd17b181c19fc2a4
Vertaler <dancheran@yandex.ru>
2017-08-07 15:35:19 +0300

    Try fix CameraClickHandler on mobile

--- Assets/Scripts/Utils/CameraClickHandler.cs
+++ Assets/Scripts/Utils/CameraClickHandler.cs
@@ -15,42 +15,30 @@ namespace HauntedCity.Utils
         void Update()
         {
             Ray ray;
-            if (SystemInfo.deviceType == DeviceType.Handheld)
-            {
-                if (Input.touches.Length > 0)
-                {
-                    Touch touch = Input.GetTouch(0);
-                    if (touch.phase == TouchPhase.Began)
-                    {
-                        ray = camera.ScreenPointToRay(touch.position);
-                    }
-                    else
-                    {
-                        return;
-                    }
-                }
-                else
-                {
-                    return;
-                }
-            }
-            else
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    ray = camera.ScreenPointToRay(Input.mousePosition);
-                }
-                else
-                {
-                    return;
-                }
-            }
-
-            if (EventSystem.current.IsPointerOverGameObject())
-            {
-                return;
-            }
             
+            #if UNITY_STANDALONE || UNITY_EDITOR
+                if (!Input.GetMouseButtonDown(0))
+                {
+                    return;
+                }
+
+                ray = camera.ScreenPointToRay(Input.mousePosition);
+                if (EventSystem.current.IsPointerOverGameObject())
+                {
+                    return;
+                }
+
+            #else
+                if (Input.touches.Length <= 0) return;
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase != TouchPhase.Began) return;
+               
+                ray = camera.ScreenPointToRay(touch.position);
+                if (EventSystem.current.IsPointerOverGameObject(touch.fingerID))
+                {
+                    return;
+                }    
+            #endif
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
